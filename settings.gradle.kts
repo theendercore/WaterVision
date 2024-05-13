28f718f413c58d2abec0f18d8e46c492b6c43d9f
theendercore <theendercore@gmail.com>
2024-05-13 14:45:41 +0300

    - full 20.6 port - rewrote the main mixin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,17 @@
+rootProject.name = "WaterVision"
 pluginManagement {
     repositories {
-        maven("https://maven.fabricmc.net/") {
-            name = "Fabric"
-        }
+        maven("https://maven.fabricmc.net/") { name = "Fabric" }
+        maven("https://maven.teamvoided.org/releases")
         mavenCentral()
+        mavenLocal()
         gradlePluginPortal()
     }
-
-    val loom_version: String by settings
-    val fabric_kotlin_version: String by settings
-    plugins {
-        id("fabric-loom") version loom_version
-        id("org.jetbrains.kotlin.jvm") version
-                fabric_kotlin_version
-                    .split("+kotlin.")[1] // Grabs the sentence after `+kotlin.`
-                    .split("+")[0] // Ensures sentences like `+build.1` are ignored
+}
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
     }
 }
--- src/main/kotlin/com/theendercore/water_vision/config/WaterVisionConfig.kt
+++ src/main/kotlin/com/theendercore/water_vision/config/WaterVisionConfig.kt
@@ -1,73 +1,119 @@
 package com.theendercore.water_vision.config
 
-import com.theendercore.water_vision.LOGGER
-import com.theendercore.water_vision.MODID
+import com.theendercore.water_vision.WaterVision.MODID
+import com.theendercore.water_vision.WaterVision.id
 import dev.isxander.yacl3.api.ConfigCategory
 import dev.isxander.yacl3.api.Option
+import dev.isxander.yacl3.api.OptionDescription
 import dev.isxander.yacl3.api.YetAnotherConfigLib
 import dev.isxander.yacl3.api.controller.FloatFieldControllerBuilder
+import dev.isxander.yacl3.api.controller.IntegerFieldControllerBuilder
 import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder
-import dev.isxander.yacl3.config.ConfigEntry
-import dev.isxander.yacl3.config.ConfigInstance
-import dev.isxander.yacl3.config.GsonConfigInstance
+import dev.isxander.yacl3.config.v2.api.ConfigClassHandler
+import dev.isxander.yacl3.config.v2.api.SerialEntry
+import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.text.Text
 
-
-class WaterVisionConfig() {
-
-
-    @ConfigEntry
+@Suppress("VariableNaming", "PropertyName", "MagicNumber")
+class WaterVisionConfig {
+    @SerialEntry
     @JvmField
     var enable = true
 
-    @ConfigEntry
+    @SerialEntry
     @JvmField
     var scale_close = 25f
 
-    @ConfigEntry
+    @SerialEntry
     @JvmField
     var scale_far = 200f
 
+    @SerialEntry
+    @JvmField
+    var enableTransition = true
+
+    @SerialEntry
+    @JvmField
+    var transitionMultiplier = 0.5f
+
+    @SerialEntry
+    @JvmField
+    var transitionLeaveMultiplier = 3
+
+
     companion object {
-        val INSTANCE: ConfigInstance<WaterVisionConfig> = GsonConfigInstance
-            .createBuilder(WaterVisionConfig::class.java)
-            .setPath(FabricLoader.getInstance().configDir.resolve("$MODID.json"))
-            .build()
+        val cfgPath = FabricLoader.getInstance().configDir.resolve("$MODID.json")
+
+        val INSTANCE: ConfigClassHandler<WaterVisionConfig> =
+            ConfigClassHandler.createBuilder(WaterVisionConfig::class.java)
+                .id(id(MODID))
+                .serializer {
+                    GsonConfigSerializerBuilder.create(it)
+                        .setPath(cfgPath)
+                        .build()
+                }
+                .build()
 
         fun makeScreen(parent: Screen?): Screen {
-            LOGGER.info("Make screen")
-            return YetAnotherConfigLib.create(INSTANCE)
-            { defaults, config, builder ->
+            return YetAnotherConfigLib.create(INSTANCE) { defaults, config, builder ->
                 builder
-                    .title(genText("title"))
+                    .title(tTxt("title"))
                     .category(
                         ConfigCategory.createBuilder()
-                            .name(genText("category"))
+                            .name(tTxt("category"))
                             .options(
                                 listOf(
-                                    Option.createBuilder<Boolean>()
-                                        .name(genText("enable"))
+                                    option<Boolean>("enable")
                                         .binding(defaults.enable, { config.enable }, { config.enable = it })
-                                        .controller { TickBoxControllerBuilder.create(it) }
+                                        .controller(TickBoxControllerBuilder::create)
                                         .build(),
-                                    Option.createBuilder<Float>()
-                                        .name(genText("scale_close"))
+
+                                    option<Float>("scale.close")
                                         .binding(
                                             defaults.scale_close,
                                             { config.scale_close },
-                                            { config.scale_close = it })
-                                        .controller { FloatFieldControllerBuilder.create(it) }
+                                            { config.scale_close = it }
+                                        )
+                                        .controller(FloatFieldControllerBuilder::create)
                                         .build(),
-                                    Option.createBuilder<Float>()
-                                        .name(genText("scale_far"))
-                                        .binding(
-                                            defaults.scale_far,
+
+                                    option<Float>("scale.far")
+                                        .binding(defaults.scale_far,
                                             { config.scale_far },
-                                            { config.scale_far = it })
-                                        .controller { FloatFieldControllerBuilder.create(it) }
+                                            { config.scale_far = it }
+                                        )
+                                        .controller(FloatFieldControllerBuilder::create)
                                         .build(),
+
+                                    option<Boolean>("transition.enable")
+                                        .description(OptionDescription.of(tTxt("transition.enable.description")))
+                                        .binding(
+                                            defaults.enableTransition,
+                                            { config.enableTransition },
+                                            { config.enableTransition = it })
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build(),
+
+                                    option<Float>("transition.multiplier")
+                                        .description(OptionDescription.of(tTxt("transition.multiplier.description")))
+                                        .binding(defaults.transitionMultiplier,
+                                            { config.transitionMultiplier },
+                                            { config.transitionMultiplier = it }
+                                        )
+                                        .controller { FloatFieldControllerBuilder.create(it).min(0.01f).max(100f) }
+                                        .build(),
+
+                                    option<Int>("transition.leave.multiplier")
+                                        .description(OptionDescription.of(tTxt("transition.leave.multiplier.description")))
+                                        .binding(defaults.transitionLeaveMultiplier,
+                                            { config.transitionLeaveMultiplier },
+                                            { config.transitionLeaveMultiplier = it }
+                                        )
+                                        .controller { IntegerFieldControllerBuilder.create(it).min(1).max(8) }
+                                        .build(),
+
                                 )
                             )
                             .build()
@@ -75,8 +121,7 @@ class WaterVisionConfig() {
             }.generateScreen(parent)
         }
 
-        private fun genText(text: String): Text {
-            return Text.translatable("config.water_vision.$text")
-        }
+        private fun <T> option(name: String): Option.Builder<T> = Option.createBuilder<T>().name(tTxt(name))
+        private fun tTxt(text: String): Text = Text.translatable("config.water_vision.$text")
     }
 }
