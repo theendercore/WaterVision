40cd3fa939d7e4cf8c5e06465779c193c8ff357a
TheEnderCore <theendercore@gmail.com>
2025-04-24 14:52:33 +0300

    - 1.21.4+ port

--- src/main/java/com/theendercore/water_vision/mixin/BackgroundRendererMixin.java
+++ src/main/java/com/theendercore/water_vision/mixin/BackgroundRendererMixin.java
@@ -1,43 +1,30 @@
 package com.theendercore.water_vision.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
-import net.minecraft.entity.Entity;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.client.render.CameraSubmersionType;
+import net.minecraft.client.render.FogParameters;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import static com.theendercore.water_vision.WaterVision.config;
 
 @Debug(export = true)
 @Mixin(BackgroundRenderer.class)
 public abstract class BackgroundRendererMixin {
-    @ModifyConstant(method = "applyFog", constant = @Constant(floatValue = -8f, ordinal = 2))
-    private static float modifyFogStartUnderwater(float constant) {
-        return config.enable ? config.scaleClose : constant;
-    }
-
-    @ModifyConstant(method = "applyFog", constant = @Constant(floatValue = 96f))
-    private static float modifyFogEndUnderwater(float constant,
-                                                @Local(argsOnly = true, ordinal = 0) float viewDistance, @Local LocalRef<Entity> entity) {
-        if (config.enable) {
-            entity.set(null);
-            return config.scaleFar * viewDistance * 0.01f;
-        }
-        return constant;
-    }
-
-    @ModifyExpressionValue(method = "applyFog", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/BackgroundRenderer$FogParameters;fogEnd:F", opcode = Opcodes.GETFIELD, ordinal = 2))
-    private static float stopFogOverride(float original, Camera camera, @Local LocalRef<Entity> entity) {
-        if (config.enable) {
-            entity.set(camera.getFocusedEntity());
-            return 0f;
+    @ModifyReturnValue(method = "applyFog", at = @At("RETURN"))
+    private static FogParameters modifyFog(FogParameters original, Camera camera, @Local(argsOnly = true, ordinal = 0) float viewDistance) {
+        if (original != FogParameters.NO_FOG && config.enable && camera.getSubmersionType() == CameraSubmersionType.WATER) {
+            var entity = camera.getFocusedEntity();
+            if (entity instanceof LivingEntity living && !(living.hasStatusEffect(StatusEffects.DARKNESS) || living.hasStatusEffect(StatusEffects.BLINDNESS))) {
+                return new FogParameters(config.scaleClose, config.scaleFar * viewDistance * 0.01f,
+                        original.shape(), original.red(), original.green(), original.blue(), original.alpha());
+            }
         }
         return original;
     }
